21be082d76b9191be3703a96f7c8b49d6ede99c6
Adrian Morales <adrianmorales2635@gmail.com>
2022-12-29 10:41:36 -0600

    added functionality to the 'contact me'

--- flask_app/controllers/main.py
+++ flask_app/controllers/main.py
@@ -1,7 +1,18 @@
 from flask_app import app
 from flask import Flask, request, session, render_template, flash, redirect
+from flask_app.models.email_send import Email
 
 @app.route('/')
 def dashboard():
     return render_template('home.html')
 
+@app.route('/send_email', methods = ['POST'])
+def sending_email():
+    data = {
+        'subject' : request.form['name'],
+        'body' : request.form['message'],
+        'sender' : request.form['email']
+    }
+    email = Email()
+    email.send_email(data)
+    return redirect('/')
